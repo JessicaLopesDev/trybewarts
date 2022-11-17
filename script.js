0759b672e86ade22e77b7eda41034ede989adfd6
audrey <audrey.gusso@gmail.com>
2022-11-17 15:48:16 -0300

    requisito 3 trabalhando

---
+++ script.js
@@ -0,0 +1,9 @@
+const inputEmail = document.querySelector('#input-email').value;
+const inputSenha = document.querySelector('#input-senha').value;
+const buttonEntrar = document.querySelector('#form-button');
+
+if (inputEmail === 'tryber@teste.com' && inputSenha === '123456') {
+    alert('Olá, Tryber!')
+
+}
+console.log(inputEmail)
