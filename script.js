b1df4630444c070caec09db97936aacc317be36a
Jessica Lopes <jessicalopes.dev@gmail.com>
2022-11-19 19:08:26 -0300

    Começando requisito 21 bonus

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const buttonEntrar = document.querySelector('#form-button');
 const inputInfo = document.querySelector('#agreement');
 const textareaInput = document.querySelector('#textarea');
 const counter = document.querySelector('#counter');
+const form = document.querySelector('#evaluation-form');
+const formContainer = document.querySelector('#form-container');
+const infoContainer = document.querySelector('#form-data');
 
 function handleSubmit(event) {
   const submitButton = document.querySelector('#submit-btn');
@@ -26,6 +29,16 @@ function handleTextareaChange(event) {
   counter.innerHTML -= event.target.value.length;
 }
 
+function handleSumitForm(event) {
+  event.preventDefault();
+  const formElements = document.querySelector('#evaluation-form').elements;
+  const nameInfo = formElements[3].name;
+  console.log(nameInfo);
+  formContainer.style.display = 'none';
+  infoContainer.style.display = 'flex';
+}
+
 buttonEntrar.addEventListener('click', handleAlert);
 inputInfo.addEventListener('click', handleSubmit);
 textareaInput.addEventListener('keyup', handleTextareaChange);
+form.onsubmit = handleSumitForm;
