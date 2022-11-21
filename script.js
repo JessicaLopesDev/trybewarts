41c7a74a776ff917e0b50ac142832a335386dad5
Jessica Lopes <jessicalopes.dev@gmail.com>
2022-11-21 19:38:12 -0300

    Requisito 21 quase resolvido

--- script.js
+++ script.js
@@ -28,28 +28,51 @@ function handleTextareaChange(event) {
   counter.innerHTML = 500;
   counter.innerHTML -= event.target.value.length;
 }
+// const evaluationForm = document.querySelector('#evaluation-form').elements;
+// document.querySelector('#name-data').innerHTML = `Nome: ${evaluationForm['input-name'].value} 
+//   ${evaluationForm['input-lastname'].value}`;
+// document.querySelector('#email-data').innerHTML = `Email: ${evaluationForm['input-email'].value}`;
+// const houses = document.querySelector('#house');
+// const selectedHouse = houses.options[houses.selectedIndex].value;
+// document.querySelector('#house-data').innerHTML = `Casa: ${selectedHouse}`;
+
+// const formFamily = document.querySelectorAll('input[name="family"]');
+// let result = '';
+// for (let index = 0; index < formFamily.length; index += 1) {
+//   if (formFamily[index].checked) {
+//     result = formFamily[index].value;
+//   }
+// }
+// document.querySelector('#family-data').innerHTML = `Casa: ${result}`;
+
+// const formTech = document.querySelectorAll('input[class="subject"]');
+// // console.log(formTech);
+// let result = '';
+// for (let index = 0; index < formTech.length; index += 1) {
+//   if (formTech[index].checked) {
+//     result += `${formTech[index].value}, `;
+//   }
+// }
+// document.querySelector('#tech-data').innerHTML = `Matérias: ${result}`;
 
 function handleSumitForm(event) {
   event.preventDefault();
   formContainer.style.display = 'none';
   infoContainer.style.display = 'flex';
 
-  const evaluationForm = document.querySelector('#evaluation-form').elements;
-  document.querySelector('#name-data').innerHTML = `Nome: ${evaluationForm['input-name'].value} 
-    ${evaluationForm['input-lastname'].value}`;
-  document.querySelector('#email-data').innerHTML = `Email: ${evaluationForm['input-email'].value}`;
-
-  // const selectedFamily = '';
-  const houses = document.querySelector('#house');
-  const selectedHouse = houses.options[houses.selectedIndex].value;
-
-  document.querySelector('#house-data').innerHTML = `Casa: ${selectedHouse}`;
+  const formRate = document.querySelectorAll('input[name="rate"]');
+  let result = '';
+  for (let index = 0; index < formRate.length; index += 1) {
+    if (formRate[index].checked) {
+      result = formRate[index].value;
+    }
+  }
+  document.querySelector('#family-data').innerHTML = `Avaliação: ${result}`;
 }
 
 
-// const formFamily = document.querySelectorAll('input[name="family"]').checked;
-// const dataFamily = document.querySelector('#family-data');
-// dataFamily.innerText = `Casa: ${formFamily.value}`;
+// const dataFamily = ;
+// dataFamily.innerText = ;
 
 buttonEntrar.addEventListener('click', handleAlert);
 inputInfo.addEventListener('click', handleSubmit);
